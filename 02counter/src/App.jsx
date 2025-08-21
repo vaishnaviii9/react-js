a64a9c9b0a6ccf42985e3beb7a8e92aa12c30cc8
Vaishnavi <vaishnavitonpe0@gmail.com>
2025-08-21 07:27:15 -0400

    add remove value function

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -7,14 +7,19 @@ function App() {
 
   const addValue = () => {
     setCount(count + 1);
+    // setCount(count + 1);
+    // setCount(count + 1);
+    // setCount(count + 1);
   };
-  
+const removeValue=()=>{
+  setCount(count-1)
+}
   return (
     <>
       <h1>React course on udemy</h1>
       <h2>Counter value:{count}</h2>
       <button onClick={addValue}>Add value</button>{" "}
-      <button>Remove value</button>
+      <button onClick={removeValue}>Remove value</button>
       <p>Footer: {count}</p>
     </>
   );
